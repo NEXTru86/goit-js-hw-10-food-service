d8d47e24e08c80e444a9d6b201676728dd15cf3e
NEXTru86 <kharinpav86@gmail.com>
2021-03-16 21:18:32 +0200

    Пушу исходный код, надеюсь ничего не сломается

--- src/menu.js
+++ src/menu.js
@@ -1,12 +1,12 @@
 import cardMenuTpl from './templates/cards.hbs';
 import cards from './menu.json';
-console.log(cards)
 import './styles.css';
 
 const cardsMenuContainer = document.querySelector('.js-menu');
 const cardsMarkup = createCardsMenu(cards);
 
 cardsMenuContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
 
 function createCardsMenu(cards) {
 
